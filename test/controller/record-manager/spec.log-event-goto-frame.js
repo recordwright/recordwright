1c93629f694bea0ad7497709d0513a239ebc47e0
Wix <longfeibisheng@gmail.com>
2022-11-29 21:23:09 -0500

    add timeout to make switch to frame unit test more stable

--- test/controller/record-manager/spec.log-event-goto-frame.js
+++ test/controller/record-manager/spec.log-event-goto-frame.js
@@ -42,7 +42,7 @@ describe('Resource Manager - stepControl - gotoFrame', () => {
         await recordManager.browserControl.activePage.click(Locator.input.locator)
         await new Promise(resolve => setTimeout(resolve, 50))
         await recordManager.browserControl.activePage.frame('test1').click(Locator.input.locator)
-        await new Promise(resolve => setTimeout(resolve, 50))
+        await new Promise(resolve => setTimeout(resolve, 150))
         assert.deepEqual(recordManager.stepControl._rawStepRepo.length, 6)
         //for step 1 to 3, the iframe should be empty because they are in main frame
         for (let i = 0; i < 3; i++) {
